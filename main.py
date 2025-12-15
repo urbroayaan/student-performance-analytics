f8d897fff9a73128859389d0486269d04a5a0875
Ayaan Anildutt <144311315+urbroayaan@users.noreply.github.com>
2025-12-15 21:27:53 +0530

    Refactor code into modular functions with main entry point

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 def calculateAvg(marks):
+
     return sum(marks) / len(marks)
 
-students = {}
-with open("students.csv","r") as file:
-    lines = file.readlines()
+def loadStudents(filename):
 
-for line in lines[1:]:
-    parts = line.strip().split(",")
+    students = {}
+    with open("students.csv","r") as file:
+        lines = file.readlines()
 
-    name = parts[0]
-    marks = list(map(int, parts[1:]))
-    students[name] = marks
+    for line in lines[1:]:
+        parts = line.strip().split(",")
 
-topper = []
-highest_avg = 0
+        name = parts[0]
+        marks = list(map(int, parts[1:]))
+        students[name] = marks
+    
+    return students
 
-for name, marks in students.items():
-    avg = calculateAvg(marks)
+def findToppers(students):
 
-    if (avg > highest_avg):
-        highest_avg = avg
-        topper = [name]
+    topper = []
+    highest_avg = 0
 
-    elif (avg == highest_avg):
-        topper.append(name)
-        highest_avg = avg
+    for name, marks in students.items():
+        avg = calculateAvg(marks)
+
+        if (avg > highest_avg):
+            highest_avg = avg
+            topper = [name]
+
+        elif (avg == highest_avg):
+            topper.append(name)
 
     print(f"Name: {name}    Average Marks: {avg}")
+    return topper, highest_avg
 
-print()
+def main():
+    students = loadStudents("students.csv")
+    toppers, highestAvg = findToppers(students)
 
-if len(topper) == 1:
-    print("Topper:", topper[0], "with an average mark of", highest_avg)
-else:
-    print("Multiple Toppers: ",", ".join(topper), "with an average mark of", highest_avg)
+    print()
+
+
+    if len(toppers) == 1:
+        print("Topper:", toppers[0], "with an average mark of", highestAvg)
+    else:
+        print("Multiple Toppers: ",", ".join(toppers), "with an average mark of", highestAvg)
+
+main()
